a80f806e9028e5ea16e01357d25d655718c40615
Andrea Mac <aleatrengrouse@gmail.com>
2023-10-15 23:06:07 +0100

    finished NavBar component

--- data-analytics/src/components/Navbar.jsx
+++ data-analytics/src/components/Navbar.jsx
@@ -10,17 +10,18 @@ const handleNav = ()=> {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px]mx- px-4 text-white'>
 <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT!</h1>
-<ul className='flex'>
+<ul className='hidden md:flex'>
     <li className='p-4'>Company</li>
     <li className='p-4'>Home</li>
     <li className='p-4'>Resources</li>
     <li className='p-4'>About</li>
     <li className='p-4'>Contact</li>
 </ul>
-<div>
-    <AiOutlineMenu size={20}/>
+<div onClick= {handleNav} className= "block md:hidden">
+    {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20}/>}
+    
 </div>
-<div className="fixed left-0 top-0 w-[60%]h-full border-r border-gray-300 bg-slate-600 ">
+<div className={!nav ? "fixed left-0 top-0 w-[60%]h-full border-r border-gray-300 bg-slate-600 ease-in-out duration-500 ": "fixed left-[-100%] "}>
 <h1 className='w-full text-3xl font-bold text-[#00df9a]m-4'>REACT!</h1>
 <ul className='p-4 uppercase '>
     <li className='p-4 border-b border-gray-300'>Company</li>
